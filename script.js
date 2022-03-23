902ece3eb54af4f257410d5b0c1422890186a1fb
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-03-23 12:44:02 -0300

    final

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("#items").addEventListener("wheel", event=> {
+    if(event.deltaY > 0) {/*targat é quem está fisparando o evento que nesse caso é o items */
+    console.log(event)
+        event.target.scrollBy(300, 0)
+    } else {
+        event.target.scrollBy(-300,0)
+    
+    }
+})
